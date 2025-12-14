d9daae64f36738b763837ed942ee5c479112764b
haoz20 <zhaonianhao2005@gmail.com>
2025-12-14 14:06:25 +0700

    intial commit

---
+++ src/CPUSchedulingSimulator.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class CPUSchedulingSimulator {
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        simulateScheduling();
+    }
+
+    public static void simulateScheduling() {
+        System.out.println("\t\tCPU SCHEDULING");
+        System.out.println("\t\t=============");
+        System.out.println();
+        System.out.println("Input the number of processes <max 15>: ");
+        System.out.println();
+
+
+    }
+
+
+}
