80c08ee706a0cfb9e63e2738519ac851a006d781
haoz20 <zhaonianhao2005@gmail.com>
2025-12-14 14:30:25 +0700

    add Process class with attributes and methods for process management

---
+++ src/Process.java
@@ -0,0 +1,55 @@
+public class Process {
+    private int processID;
+    private int arrivalTime;
+    private int burstTime;
+    private int priority;
+    private int waitingTime;
+
+    public Process(int processID, int burstTime, int priority, int arrivalTime) {
+        this.processID = processID;
+        this.burstTime = burstTime;
+        this.priority = priority;
+        this.arrivalTime = arrivalTime;
+        this.waitingTime = 0;
+    }
+
+    public int getProcessID() {
+        return processID;
+    }
+
+    public void setProcessID(int processID) {
+        this.processID = processID;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public void setBurstTime(int burstTime) {
+        this.burstTime = burstTime;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getWaitingTime() {
+        return waitingTime;
+    }
+
+    public void setWaitingTime(int waitingTime) {
+        this.waitingTime = waitingTime;
+    }
+}
